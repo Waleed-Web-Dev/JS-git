6ac5c668b6166303be5127f6daf1bf94b032e6b9
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-05-30 04:14:56 +0000

    CONVERISON OPERATIONS

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -25,4 +25,39 @@ let val = Boolean(isLogged)
 let num = 42;
 let st = String(num);
 console.log(st);
-console.log(typeof st);s
+console.log(typeof st);
+
+// ************Operations****************
+
+let val1 = 35;
+let val2 = -val1;
+
+console.log(val2);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+//Alll the above are basic operations, the confusion occurs in following
+
+console.log("1" + 3 +3); //Output:133; Beacuse first is tsring all the latters are also converted to string
+console.log(1 + 2 + "3"); //Outpur: 33; As string is at last, so first opeartion is peformed then it is
+// concatenated
+
+let str1 = "Waleed";
+let str2 = " Ahmed";
+
+console.log(str1 + str2);
+
+let gameCounter = 199;
+
+++gameCounter;
+
+console.log(gameCounter); //Output: 200
+
+
+console.log(+true); //Output: 1 
+console.log(+"");//Output: 0
