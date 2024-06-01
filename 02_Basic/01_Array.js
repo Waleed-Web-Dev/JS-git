83081a4c359e790c71330108d6cba1d8a82f9f91
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-01 06:50:37 +0000

    array 1

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,32 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+let arr1 = ["waleed", "Ahmed"];
+
+// console.log(arr[0]);
+
+// //Array Method
+
+// arr.push(6);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(0);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+/*Push last men elemnet ku dalta he aur pop last se nikalta he jabke unshift shuru men dalta he aur shift last se 
+nikalta he */
+
+// console.log(arr.includes(23));
+
+//slice , splice
+
+console.log(arr.slice(2,5));
+
+console.log(arr.splice(2,5))
+console.log(arr)
+
+/*Dekhiye splice origianl array ku bhi change kar deta he jabke slice nhi karta */
