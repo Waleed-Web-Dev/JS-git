a42a69dac91c66f0fe2cf5a29fe2e49bc8d99f0b
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-02 05:42:25 +0000

    Second Array commit

---
+++ 02_Basic/02_Array.js
@@ -0,0 +1,25 @@
+let arr1 = [1,2,3,4,5];
+let arr2 = [6,7,8,9,10];
+
+// arr2.push(arr1);
+
+// console.log(arr2);
+
+// let arr3 = arr2.concat(arr1);
+// console.log(arr3);
+
+// let arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+// let arr3 = [1,2,3,4,[5,6,7], 8,9,[10,11,[12,4]]];
+
+// let arr4 = arr3.flat(Infinity)
+
+// console.log(arr4);
+
+
+console.log(Array.isArray("Waleed"));
+console.log(Array.from("Waleed"));
+console.log(Array.from({name: "Waleed"}));
+
+console.log(Array.of("Waleed"))
