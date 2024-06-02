a5b99cfa457b5b04e92ca2159b395f1f5e658fa3
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-02 20:10:35 +0000

    Scoping voping hu rha he yahan

---
+++ 03_Basic.js/02_Scope.js
@@ -0,0 +1,12 @@
+//Yara bakit upta he scope wope kese huta he yeh neche wali chez interesting he
+
+addone(5)
+function addone(num){
+    return num+1
+}
+
+
+add2(5)
+const add2 = function (num){
+    return num+2
+}
