e1cf562ed754520be20ee9e352067b5e26ddb805
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-04 21:56:57 +0000

    Do while

--- 05_Iterations/two.js
+++ 05_Iterations/two.js
@@ -1,13 +1,19 @@
 let arr = ["me", "you", "he", "she", "they"];
 let i = 0;
-while (i < arr.length) {
+// while (i < arr.length) {
+//     console.log(`${arr[i]}`);
+//     i++;
+// }
+
+
+// const obj = {};
+
+// if (Object.keys(obj).length == 0) {
+//     console.log("Object is empty")
+// }
+
+
+do {
     console.log(`${arr[i]}`);
-    i++;
-}
-
-
-const obj = {};
-
-if (Object.keys(obj).length == 0) {
-    console.log("Object is empty")
-}
+     i++;
+} while (i < arr.length);
