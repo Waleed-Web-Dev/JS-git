a442daf17ce33176493f7fc03c196342eb8cbe93
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-04 21:55:34 +0000

    While and Do while loop

---
+++ 05_Iterations/two.js
@@ -0,0 +1,13 @@
+let arr = ["me", "you", "he", "she", "they"];
+let i = 0;
+while (i < arr.length) {
+    console.log(`${arr[i]}`);
+    i++;
+}
+
+
+const obj = {};
+
+if (Object.keys(obj).length == 0) {
+    console.log("Object is empty")
+}
