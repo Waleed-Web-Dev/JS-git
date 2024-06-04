5d30919f66c8b427bc3a5c672bb806ec14c83799
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-04 21:33:43 +0000

    Iteration

---
+++ 05_Iterations/one.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i <= 20; i++) {
+//     const element = i;
+//     if (i == 5) {
+//         console.log('5 Detected');
+//         break;
+//     }
+//     console.log(`Value of i is ${i}`);
+    
+// }
+
+// console.log('Out of loop');
+
+let myarr = ["waleed", "anas", "anousha"];
+for (let index = 0; index < myarr.length; index++) {
+    const element = myarr[index];
+    console.log(element);
+}
