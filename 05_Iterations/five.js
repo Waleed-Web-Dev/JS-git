df44a39a773763109de7b6689bb8cbd433face8b
Waleed-Web-Dev <126647130+Waleed-Web-Dev@users.noreply.github.com>
2024-06-27 05:29:29 +0000

    for each

---
+++ 05_Iterations/five.js
@@ -0,0 +1,28 @@
+//for Each
+let arr = [43,35,3535,33,5];
+
+arr.forEach( (value, index, arr)=> {
+    console.log(`Value at index ${index} is ${value} and the whole array is ${arr}`);
+});
+
+function con(kk) {
+    console.log(kk);
+}
+
+// arr.forEach(con)
+
+let arr2 = [
+    {
+        prog: "jaj",
+        kdgjbs: "Af"
+    },
+    {
+        prog: "DFg",
+        kdgjbs: "FA"
+    }
+]
+
+
+arr2.forEach((ind) => {
+    // console.log(ind.prog)
+})
